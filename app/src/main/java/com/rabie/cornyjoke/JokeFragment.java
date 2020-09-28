0358ca89a77dff8f420a7846270e8b546d005217
ahmed.rabie.moussa <ahmed.rabie.moussa@gmail.com>
2020-09-28 18:32:05 +0200

    second commit

--- app/src/main/java/com/rabie/cornyjoke/JokeFragment.java
+++ app/src/main/java/com/rabie/cornyjoke/JokeFragment.java
@@ -4,13 +4,16 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
 public class JokeFragment extends Fragment {
+    JokeModel joke;
+
     public JokeFragment(JokeModel joke) {
-
-
+        this.joke = joke;
     }
 
     @Override
@@ -20,7 +23,14 @@ public class JokeFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fetched_joke_fragment, container, false);
+        View fragmentView = inflater.inflate(R.layout.fetched_joke_fragment, container, false);
+        TextView presentedJoke = (TextView) fragmentView.findViewById(R.id.presented_joke);
+        presentedJoke.setText(joke.getJoke_content());
+        Button minusButton = (Button) fragmentView.findViewById(R.id.minus_button);
+        minusButton.setText(String.valueOf(joke.getJoke_downvotes()));
+        Button plusButton = (Button) fragmentView.findViewById(R.id.plus_button);
+        plusButton.setText(String.valueOf(joke.getJoke_upvotes()));
+        return fragmentView;
     }
 
     @Override
--- app/src/main/java/com/rabie/cornyjoke/JokesActivity.java
+++ app/src/main/java/com/rabie/cornyjoke/JokesActivity.java
@@ -1,8 +1,11 @@
 package com.rabie.cornyjoke;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class JokesActivity extends AppCompatActivity {
 
@@ -10,6 +13,15 @@ public class JokesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.jokes_screen);
+    }
+
+    public void getAJoke(View view) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        JokeFragment jokeFragment = new JokeFragment(new JokeModel(1, "ed7ak b2a ya sa7by batal 8abawa", 0, 0));
+        fragmentTransaction.replace(R.id.upperJokeFragment, jokeFragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
     }
 
 }
